86f13dbe50ad2c2bf031d6b27201e7ccd5ef95f7
cpozo94 <cpg.948@gmail.com>
2023-03-02 11:30:54 +0000

    adding files

---
+++ src/js/component/Traffic.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+
+const Traffic = () => {
+
+
+    return (
+        <div className="container">
+        <div className="semaforo"> 
+            <div className="luces" id="red"></div>
+            <div className="luces" id="orange"></div>
+            <div className="luces" id="green"></div>
+        </div>
+        </div>
+
+    )
+}
+
+export default Traffic;
